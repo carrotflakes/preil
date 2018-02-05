95e4f44b1edb6d4d47ca5661b105a399276bbd99
carrotflakes <carrotflakes@gmail.com>
2018-02-05 22:28:45 +0900

    Fix test

--- t/preil.lisp
+++ t/preil.lisp
@@ -9,29 +9,31 @@
 
 (plan nil)
 
-(with-world ()
 
-  (prelude:import-definition)
+(defvar world
+  (create-world
+    (import-world :prelude)))
 
-  (is (solve-all ?x '(append (1 2) (3 4) ?x))
-      '((1 2 3 4))
-      :test #'equal)
 
-  (is (solve-all ?x '(format ?x "~a-~b" a 1))
-      '("A-1")
-      :test #'equal)
+(is (solve-all world ?x '(append (1 2) (3 4) ?x))
+  '((1 2 3 4))
+  :test #'equal)
 
-  (is (solvep '(< 1 2))
-      t
-      :test #'equal)
+(is (solve-all world ?x '(format ?x "~a-~b" a 1))
+  '("A-1")
+  :test #'equal)
 
-  (is (solve-1 ?x '(prelude:find-1 (?x) (x) (member x (1 2 3))))
-      '1
-      :test #'equal)
+(is (solvep world '(< 1 2))
+  t
+  :test #'equal)
 
-  (is (solve-1 ?x '(prelude:find-all ?x (x y) (append x y (1 2 3))))
-      '((() (1 2 3)) ((1) (2 3)) ((1 2) (3)) ((1 2 3) ()))
-      :test #'equal)
-  )
+(is (solve-1 world ?x '(prelude:find-1 (?x) (x) (member x (1 2 3))))
+  '1
+  :test #'equal)
+
+(is (solve-1 world ?x '(prelude:find-all ?x (x y) (append x y (1 2 3))))
+  '((() (1 2 3)) ((1) (2 3)) ((1 2) (3)) ((1 2 3) ()))
+  :test #'equal)
+
 
 (finalize)
